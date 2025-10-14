4c8106bdf1c8b8e74e4f7d5e72b7027912882593
sr410 <sukhmanjeet.rekhi@ontariotechu.net>
2025-10-14 17:26:07 -0400

    letter grade

--- grade.py
+++ grade.py
@@ -3,7 +3,7 @@
 #=============================
 
 #this is the score
-score =float (input("enter your score:"))
+score = float(input("enter your score:"))
 #this is the total points
 tp =int (input("enter your total points:"))
 
@@ -13,7 +13,21 @@ tgrade= float (score / tp * 100)
 #round grade
 grade = round(tgrade, 2)
 
-print("Your grade is", grade,"%")
+#Letter grade
+
+if grade >= 90:
+    letter = "A"
+elif grade >= 80:
+    letter = "B"
+elif grade >= 70:
+    letter = "C"
+elif grade >= 60:
+    letter = "D"
+else:
+    letter = "F"
+
+
+print("Your grade is", grade,"%,", "which is a", letter)
 
 
 
